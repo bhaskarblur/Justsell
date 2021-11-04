5cb3088c68f3ac2baab14796f24b7c15d2a4c861
Bhaskar <bvkaura@gmail.com>
2021-11-04 13:57:36 +0530

    2nd Update

--- app/src/main/java/com/classified/justsell/HomeActivity.java
+++ app/src/main/java/com/classified/justsell/HomeActivity.java
@@ -1,17 +1,213 @@
 package com.classified.justsell;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.classified.justsell.Fragments.chatFragment;
+import com.classified.justsell.Fragments.homeFragment;
+import com.classified.justsell.Fragments.postFragment;
+import com.classified.justsell.Fragments.profileFragment;
 import com.classified.justsell.R;
+import com.classified.justsell.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
-
+    final int PERMISSION_CODE = 1001;
+    ActivityHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding=ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         this.getSupportActionBar().hide();
+
+        LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+             //startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+        }
+        try {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        handlePermissions();
+        handleBottomNav();
+    }
+
+    private void handlePermissions() {
+
+    }
+
+    private void handleBottomNav() {
+        binding.homeicon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.homeicon.setVisibility(View.INVISIBLE);
+                binding.homeselectedicon.setVisibility(View.VISIBLE);
+
+                binding.chaticon.setVisibility(View.VISIBLE);
+                binding.chatselectedicon.setVisibility(View.INVISIBLE);
+
+                binding.profileicon.setVisibility(View.VISIBLE);
+                binding.profileselectedicon.setVisibility(View.INVISIBLE);
+
+                binding.posticon.setVisibility(View.VISIBLE);
+                binding.postselectedicon.setVisibility(View.INVISIBLE);
+
+                homeFragment homeFragment=new homeFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.setCustomAnimations(R.anim.fade_2, R.anim.fade);
+                transaction.replace(R.id.mainFragment, homeFragment);
+                transaction.commit();
+            }
+        });
+
+        binding.posticon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.homeicon.setVisibility(View.VISIBLE);
+                binding.homeselectedicon.setVisibility(View.INVISIBLE);
+
+                binding.chaticon.setVisibility(View.VISIBLE);
+                binding.chatselectedicon.setVisibility(View.INVISIBLE);
+
+                binding.profileicon.setVisibility(View.VISIBLE);
+                binding.profileselectedicon.setVisibility(View.INVISIBLE);
+
+                binding.posticon.setVisibility(View.INVISIBLE);
+                binding.postselectedicon.setVisibility(View.VISIBLE);
+
+                postFragment homeFragment=new postFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.setCustomAnimations(R.anim.fade_2, R.anim.fade);
+                transaction.replace(R.id.mainFragment, homeFragment);
+                transaction.commit();
+            }
+        });
+
+        binding.chaticon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.homeicon.setVisibility(View.VISIBLE);
+                binding.homeselectedicon.setVisibility(View.INVISIBLE);
+
+                binding.chaticon.setVisibility(View.INVISIBLE);
+                binding.chatselectedicon.setVisibility(View.VISIBLE);
+
+                binding.profileicon.setVisibility(View.VISIBLE);
+                binding.profileselectedicon.setVisibility(View.INVISIBLE);
+
+                binding.posticon.setVisibility(View.VISIBLE);
+                binding.postselectedicon.setVisibility(View.INVISIBLE);
+
+                chatFragment homeFragment=new chatFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.setCustomAnimations(R.anim.fade_2, R.anim.fade);
+                transaction.replace(R.id.mainFragment, homeFragment);
+                transaction.commit();
+            }
+        });
+
+        binding.profileicon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.homeicon.setVisibility(View.VISIBLE);
+                binding.homeselectedicon.setVisibility(View.INVISIBLE);
+
+                binding.chaticon.setVisibility(View.VISIBLE);
+                binding.chatselectedicon.setVisibility(View.INVISIBLE);
+
+                binding.profileicon.setVisibility(View.INVISIBLE);
+                binding.profileselectedicon.setVisibility(View.VISIBLE);
+
+                binding.posticon.setVisibility(View.VISIBLE);
+                binding.postselectedicon.setVisibility(View.INVISIBLE);
+
+                profileFragment homeFragment=new profileFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.setCustomAnimations(R.anim.fade_2, R.anim.fade);
+                transaction.replace(R.id.mainFragment, homeFragment);
+                transaction.commit();
+            }
+        });
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (getSupportFragmentManager().getBackStackEntryCount() != 0) {
+            getSupportFragmentManager().popBackStack();
+
+            if (getSupportFragmentManager().getBackStackEntryCount() < 2) {
+                // set the home tab as default;
+                binding.homeicon.setVisibility(View.INVISIBLE);
+                binding.homeselectedicon.setVisibility(View.VISIBLE);
+
+                binding.chaticon.setVisibility(View.VISIBLE);
+                binding.chatselectedicon.setVisibility(View.INVISIBLE);
+
+                binding.profileicon.setVisibility(View.VISIBLE);
+                binding.profileselectedicon.setVisibility(View.INVISIBLE);
+
+                binding.posticon.setVisibility(View.VISIBLE);
+                binding.postselectedicon.setVisibility(View.INVISIBLE);
+
+                homeFragment homeFragment=new homeFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.setCustomAnimations(R.anim.fade_2, R.anim.fade);
+                transaction.replace(R.id.mainFragment, homeFragment);
+                transaction.commit();
+
+            }
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this).
+                    setTitle("Exit?").setMessage("Do you want to exit the app?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+            builder.show();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_CODE: {
+
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+
+                } else {
+                    Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        }
     }
 }
---
+++ app/src/main/java/com/classified/justsell/helpingCode/helpingcode.java
@@ -0,0 +1,272 @@
+package com.classified.justsell.helpingCode;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.Observer;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class helpingcode {
+
+
+    // getting location code
+//    private String lat;
+//    private String longit;
+//    private LocationManager locationManager;
+//    private FusedLocationProviderClient fusedLocationProviderClient;
+//    private String mLastLocation;
+//    @SuppressLint("MissingPermission")
+//    private void getlatlong() {
+//        locationManager = (LocationManager) getActivity().getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+//        if (!locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+//            getActivity().startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+//        }
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                if(dataloaded.equals(false)) {
+//                    dataloaded=true;
+//                    hmbinding.homefragscroll.setVisibility(View.INVISIBLE);
+//                    hmbinding.progressBar2.setVisibility(View.INVISIBLE);
+//                    hmbinding.retrybtn.setVisibility(View.VISIBLE);
+//                    hmbinding.rettxt.setVisibility(View.VISIBLE);
+//
+//                }
+//            }
+//        },6000);
+//        if(getContext()!=null) {
+//            if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//
+//                fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+//                LocationRequest request = new LocationRequest().setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+//                        .setInterval(10000).setFastestInterval(1000).setNumUpdates(1);
+//                fusedLocationProviderClient.requestLocationUpdates(request, new LocationCallback() {
+//                    @Override
+//                    public void onLocationResult(LocationResult locationResult) {
+//                        super.onLocationResult(locationResult);
+//                        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<Location> task) {
+//                                if (dataloaded.equals(false)) {
+//                                    Location location = task.getResult();
+//                                    if (location != null) {
+//                                        dataloaded = true;
+//                                        lat = String.valueOf(location.getLatitude());
+//                                        longit = String.valueOf(location.getLongitude());
+//                                        hmViewModel.getlocation(lat, longit);
+//                                        Geocoder geocoder = null;
+//                                        if(getContext()!=null) {
+//                                            geocoder = new Geocoder(getActivity()
+//                                                    , Locale.getDefault());
+//                                        }
+//                                        try {
+//                                            if(geocoder!=null) {
+//                                                List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+//
+//                                                hmbinding.locattext.setText(addresses.get(0).getLocality());
+//                                            }
+//                                        } catch (IOException e) {
+//                                            e.printStackTrace();
+//                                        }
+//
+//                                        if(hmViewModel.getnbyshopModel()!=null) {
+//                                            hmViewModel.getnbyshopModel().observe(getActivity(), new Observer<List<nbyshopsModel>>() {
+//                                                @Override
+//                                                public void onChanged(List<nbyshopsModel> nbyshopsModels) {
+//                                                    new Handler().postDelayed(new Runnable() {
+//                                                        @Override
+//                                                        public void run() {
+//                                                            if (nbyshopsModels.size() > 0) {
+//                                                                nbadapter.notifyDataSetChanged();
+//                                                                hmbinding.homefragscroll.setVisibility(View.VISIBLE);
+//                                                                hmbinding.progressBar2.setVisibility(View.INVISIBLE);
+//                                                            }
+//                                                        }
+//                                                    }, 100);
+//                                                }
+//                                            });
+//                                            loadnearbyshoprec();
+//                                        }
+//                                    } else {
+//
+//                                        LocationRequest request = new LocationRequest().setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+//                                                .setInterval(10000).setFastestInterval(1000).setNumUpdates(1);
+//
+//                                        LocationCallback locationCallback = new LocationCallback() {
+//                                            @Override
+//                                            public void onLocationResult(LocationResult locationResult) {
+//                                                super.onLocationResult(locationResult);
+//                                                Location location1 = locationResult.getLastLocation();
+//                                                lat = String.valueOf(location1.getLatitude());
+//                                                longit = String.valueOf(location1.getLongitude());
+//                                                hmViewModel.getlocation(lat, longit);
+//                                                Geocoder geocoder = null;
+//                                                if(getContext()!=null) {
+//                                                    geocoder = new Geocoder(getActivity()
+//                                                            , Locale.getDefault());
+//                                                }
+//                                                try {
+//                                                    List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+//
+//                                                    hmbinding.locattext.setText(addresses.get(0).getLocality());
+//                                                } catch (IOException e) {
+//                                                    e.printStackTrace();
+//                                                }
+//
+//                                                hmViewModel.getnbyshopModel().observe(getActivity(), new Observer<List<nbyshopsModel>>() {
+//                                                    @Override
+//                                                    public void onChanged(List<nbyshopsModel> nbyshopsModels) {
+//                                                        new Handler().postDelayed(new Runnable() {
+//                                                            @Override
+//                                                            public void run() {
+//                                                                if (nbyshopsModels.size() > 0) {
+//                                                                    nbadapter.notifyDataSetChanged();
+//                                                                    hmbinding.homefragscroll.setVisibility(View.VISIBLE);
+//                                                                    hmbinding.progressBar2.setVisibility(View.INVISIBLE);
+//                                                                }
+//                                                            }
+//                                                        }, 100);
+//                                                    }
+//                                                });
+//                                                loadnearbyshoprec();
+//                                            }
+//                                        };
+//                                    }
+//                                }
+//                            }
+//                        }).addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                // Toast.makeText(getContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+//                            }
+//                        });
+//                    }
+//                }, Looper.getMainLooper());
+//                LocationManager manager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+//                if (!manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+//                    getActivity().startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+//                }
+//
+//
+//            }
+//        }
+//
+//    }
+
+
+    //banner rotation
+
+//    private void rotatebanner() {
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                if (bannerAdapter.getItemCount() > 0) {
+//                    if (bannerAdapter.getItemCount() > hmbinding.bannerrv.getCurrentItem() && hmbinding.bannerrv.getCurrentItem() == 0) {
+//                        hmbinding.bannerrv.setCurrentItem(1, true);
+//                        hmbinding.onbprog2.getBackground().setTint(Color.parseColor("#0881E3"));
+//                        hmbinding.onbprog1.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        hmbinding.onbprog3.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        hmbinding.onbprog4.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        rotatebanner();
+//                        return;
+//                    }
+//                    if (bannerAdapter.getItemCount() > hmbinding.bannerrv.getCurrentItem() && hmbinding.bannerrv.getCurrentItem() == 1) {
+//                        hmbinding.bannerrv.setCurrentItem(2, true);
+//                        hmbinding.onbprog3.getBackground().setTint(Color.parseColor("#0881E3"));
+//                        hmbinding.onbprog1.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        hmbinding.onbprog2.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        hmbinding.onbprog4.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        rotatebanner();
+//                        return;
+//                    }
+//                    if (bannerAdapter.getItemCount() > hmbinding.bannerrv.getCurrentItem() && hmbinding.bannerrv.getCurrentItem() == 2) {
+//                        hmbinding.bannerrv.setCurrentItem(3, true);
+//                        hmbinding.onbprog4.getBackground().setTint(Color.parseColor("#0881E3"));
+//                        hmbinding.onbprog1.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        hmbinding.onbprog2.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        hmbinding.onbprog3.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        rotatebanner();
+//                        return;
+//                    }
+//
+//                    if (bannerAdapter.getItemCount() > hmbinding.bannerrv.getCurrentItem() && hmbinding.bannerrv.getCurrentItem() == 2) {
+//                        hmbinding.bannerrv.setCurrentItem(0, true);
+//                        hmbinding.onbprog1.getBackground().setTint(Color.parseColor("#0881E3"));
+//                        hmbinding.onbprog2.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        hmbinding.onbprog3.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        hmbinding.onbprog4.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        rotatebanner();
+//                        return;
+//                    }
+//                    if (hmbinding.bannerrv.getCurrentItem() == bannerAdapter.getItemCount() - 1) {
+//                        hmbinding.bannerrv.setCurrentItem(0);
+//                        hmbinding.onbprog1.getBackground().setTint(Color.parseColor("#0881E3"));
+//                        hmbinding.onbprog2.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        hmbinding.onbprog3.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        hmbinding.onbprog4.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        rotatebanner();
+//                        return;
+//
+//                    } else {
+//                        hmbinding.bannerrv.setCurrentItem(0);
+//                        hmbinding.onbprog1.getBackground().setTint(Color.parseColor("#0881E3"));
+//                        hmbinding.onbprog2.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        hmbinding.onbprog3.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        hmbinding.onbprog4.getBackground().setTint(Color.parseColor("#C6C6C6"));
+//                        rotatebanner();
+//                    }
+//                }
+//
+//
+//            }
+//        }, 5000);
+//    }
+
+
+
+//    @Override
+//    public void onLocationChanged(@NonNull Location location) {
+//
+//    }
+//
+//    @Override
+//    public void onStatusChanged(String provider, int status, Bundle extras) {
+//
+//    }
+//
+//    @Override
+//    public void onProviderEnabled(@NonNull String provider) {
+//
+//    }
+//
+//    @Override
+//    public void onProviderDisabled(@NonNull String provider) {
+//
+//    }
+
+}
