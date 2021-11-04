d6c01fe3886008a43ed57759dd2a062dccdb3291
luckydbvk2847 <bhanu=vasu1234>
2021-11-04 15:44:14 +0530

    3rd update

--- app/src/main/java/com/classified/justsell/Fragments/homeFragment.java
+++ app/src/main/java/com/classified/justsell/Fragments/homeFragment.java
@@ -9,18 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.classified.justsell.R;
+import com.classified.justsell.databinding.FragmentHomeBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link homeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class homeFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private FragmentHomeBinding binding;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -30,14 +28,6 @@ public class homeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment homeFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static homeFragment newInstance(String param1, String param2) {
         homeFragment fragment = new homeFragment();
@@ -60,7 +50,12 @@ public class homeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding=FragmentHomeBinding.inflate(inflater,container,false);
+
+        ManageUI();
+        return binding.getRoot();
+    }
+
+    private void ManageUI() {
     }
 }
