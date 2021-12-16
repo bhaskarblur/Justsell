b5d61223858c12e27c57fdf4fe266bf269931c9c
soundwave2847 <bvkaura@gmail.com>
2021-12-16 17:41:38 +0530

    fixed msg size

--- app/src/main/java/com/classified/justsell/Constants/api_baseurl.java
+++ app/src/main/java/com/classified/justsell/Constants/api_baseurl.java
@@ -2,7 +2,7 @@ package com.classified.justsell.Constants;
 
 public class api_baseurl {
 
-    public String apibaseurl= "http://demowebsites.co.in/justsell2/api/";
+    public String apibaseurl= "http://justsell.io/api/";
 
     public String apibaseurl_market= "http://lmartsolutions.com/api/";
 }
